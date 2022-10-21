0691be0990985c706475f68c322fd6e9960336aa
siyan-david <siyanboladavid147@gmail.com>
2022-10-21 06:54:48 -0700

    Simple Birthday Reminder

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'https://imgs.search.brave.com/ri57Y6qPmCXyYqhTfoVE032cTPIkBCEHR850J-Iwm_I/rs:fit:316:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5K/SlplWjNRX3hSZ3Va/bXQ5RFJXMnN3SGFM/SCZwaWQ9QXBp',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'https://imgs.search.brave.com/ri57Y6qPmCXyYqhTfoVE032cTPIkBCEHR850J-Iwm_I/rs:fit:316:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5K/SlplWjNRX3hSZ3Va/bXQ5RFJXMnN3SGFM/SCZwaWQ9QXBp',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://imgs.search.brave.com/ri57Y6qPmCXyYqhTfoVE032cTPIkBCEHR850J-Iwm_I/rs:fit:316:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5K/SlplWjNRX3hSZ3Va/bXQ5RFJXMnN3SGFM/SCZwaWQ9QXBp',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'https://imgs.search.brave.com/ri57Y6qPmCXyYqhTfoVE032cTPIkBCEHR850J-Iwm_I/rs:fit:316:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5K/SlplWjNRX3hSZ3Va/bXQ5RFJXMnN3SGFM/SCZwaWQ9QXBp',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://imgs.search.brave.com/ri57Y6qPmCXyYqhTfoVE032cTPIkBCEHR850J-Iwm_I/rs:fit:316:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5K/SlplWjNRX3hSZ3Va/bXQ5RFJXMnN3SGFM/SCZwaWQ9QXBp',
+  },
+]
